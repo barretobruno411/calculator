a5e1d83fa6fa1d5343e02c0fe40a5f19dc2edb0e
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-14 10:27:53 -0300

    Add dot restriction to prevValue component

--- js/modules/functions.js
+++ js/modules/functions.js
@@ -7,53 +7,45 @@ import {
 
 const regex_number = /[0-9]|\./;
 const regex_operator = /x|\/|\-|\+/gi;
+const regex_dot = /\./;
 
 export const displayAdd = (content) => {
-
-    if (operatorValue.textContent) {
-        console.log("ja tem valor no operador")
-    }
-
   if (regex_number.test(content)) {
-
     if (prevValue.textContent === "0") {
-
       prevValue.textContent = content;
-
     } else if (operatorValue.textContent) {
-
-        currentValue.textContent += content
-
+      currentValue.textContent += content;
     } else if (prevValue.textContent !== "0") {
-
+      if (prevValue.textContent.includes(".") && content.includes(".")) {
+        return;
+      }
       prevValue.textContent += content;
-
     }
-  } else  {
-    
+  } else {
     operatorValue.textContent = content;
-
   }
 };
 
 export const evaluate = (valueOne, operand, valueTwo) => {
-
-    if (operand.textContent === "+") {
-        prevValue.textContent = Number(valueOne.textContent) + Number(valueTwo.textContent)
-        operatorValue.textContent = ""
-        currentValue.textContent = ""
-    } else if (operand.textContent === "/") {
-        prevValue.textContent = Number(valueOne.textContent) / Number(valueTwo.textContent)
-        operatorValue.textContent = ""
-        currentValue.textContent = ""
-    } else if (operand.textContent === "-") {
-        prevValue.textContent = Number(valueOne.textContent) - Number(valueTwo.textContent)
-        operatorValue.textContent = ""
-        currentValue.textContent = ""
-    } else if (operand.textContent === "X") {
-        prevValue.textContent = Number(valueOne.textContent) * Number(valueTwo.textContent)
-        operatorValue.textContent = ""
-        currentValue.textContent = ""
-    }
-
-}
+  if (operand.textContent === "+") {
+    prevValue.textContent =
+      Number(valueOne.textContent) + Number(valueTwo.textContent);
+    operatorValue.textContent = "";
+    currentValue.textContent = "";
+  } else if (operand.textContent === "/") {
+    prevValue.textContent =
+      Number(valueOne.textContent) / Number(valueTwo.textContent);
+    operatorValue.textContent = "";
+    currentValue.textContent = "";
+  } else if (operand.textContent === "-") {
+    prevValue.textContent =
+      Number(valueOne.textContent) - Number(valueTwo.textContent);
+    operatorValue.textContent = "";
+    currentValue.textContent = "";
+  } else if (operand.textContent === "X") {
+    prevValue.textContent =
+      Number(valueOne.textContent) * Number(valueTwo.textContent);
+    operatorValue.textContent = "";
+    currentValue.textContent = "";
+  }
+};
