1e50dfd690252e59a613ff7d016b07cca86c7701
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-14 11:41:23 -0300

    Add reset and delete interface by keyboard

--- js/index.js
+++ js/index.js
@@ -17,7 +17,20 @@ buttons.forEach((button) =>
 equals.addEventListener("click", (e) => evaluate(prevValue,operatorValue,currentValue));
 
 document.addEventListener("keydown", (e) => {
+    console.log(e.key)
+    if (e.key === "Escape") {
+        prevValue.textContent = "0"
+        currentValue.textContent = ""
+        operatorValue.textContent = ""
+    }
+    if (e.key === "Backspace") {
+        if (operatorValue.textContent) {
+            currentValue.textContent = currentValue.textContent.slice(0,-1)
+        } else {
+            prevValue.textContent = prevValue.textContent.slice(0,-1)
+        }
 
+    }
     if (e.key === "Enter") {
         document.getElementById("=").click()
     }
