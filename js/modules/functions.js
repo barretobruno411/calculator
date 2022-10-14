dfabbeee9020df69777f0ba98d212bef83eccd12
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-14 09:11:39 -0300

    Add logical to evaluate if input is number or not, so atribute to respective component in display

--- js/modules/functions.js
+++ js/modules/functions.js
@@ -1,10 +1,20 @@
-import { display, prevValue, operatorValue, currentValue } from "./domModels.js";
+import {
+  display,
+  prevValue,
+  operatorValue,
+  currentValue,
+} from "./domModels.js";
+
+const regex_number = /[0-9]/;
 
 export const displayAdd = (content) => {
-  if (display.textContent === "0") {
-    display.textContent = content;
+  if (regex_number.test(content)) {
+    if (prevValue.textContent === "0") {
+      prevValue.textContent = content;
+    } else {
+      prevValue.textContent += content;
+    }
   } else {
-    display.textContent += content;
+    operatorValue.textContent = content
   }
-  
 };
